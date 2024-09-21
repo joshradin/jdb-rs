00e5eba8f9f5bf3f4493672c37544db43670ae21
joshradin <jradin16@gmail.com>
2024-09-21 15:46:08 -0700

    created jdwp-client

---
+++ crates/jdwp-client/src/id_sizes.rs
@@ -0,0 +1,43 @@
+//! Id sizes, retrieved from the VM
+
+/// Contains the data on the size of ids in use by the target JVM
+#[derive(Debug, Copy, Clone)]
+pub struct IdSizes {
+    object_id_size: usize,
+    method_id_size: usize,
+    field_id_size: usize,
+    frame_id_size: usize,
+}
+
+impl IdSizes {
+    /// Gets the size (in bytes) of object ids
+    pub fn object_id_size(&self) -> usize {
+        self.object_id_size
+    }
+
+    /// Gets the size (in bytes) of method ids
+    pub fn method_id_size(&self) -> usize {
+        self.method_id_size
+    }
+
+    /// Gets the size (in bytes) of field ids
+    pub fn field_id_size(&self) -> usize {
+        self.field_id_size
+    }
+
+    /// Gets the size (in bytes) of frame ids
+    pub fn frame_id_size(&self) -> usize {
+        self.frame_id_size
+    }
+}
+
+impl Default for IdSizes {
+    fn default() -> Self {
+        Self {
+            object_id_size: 8,
+            method_id_size: 8,
+            field_id_size: 8,
+            frame_id_size: 8,
+        }
+    }
+}
---
+++ crates/jdwp-client/src/lib.rs
@@ -0,0 +1,9 @@
+//! A basic jdwp client, this is a raw jdwp implementation that matches the original spec
+
+pub mod id_sizes;
+
+/// A non-blocking jdwp client
+#[derive(Debug)]
+pub struct JdwpClient {
+
+}
